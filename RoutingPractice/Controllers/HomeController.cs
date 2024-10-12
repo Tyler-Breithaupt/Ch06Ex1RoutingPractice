1732dc847ea8f1b5bfca78559a1e19c4c902a50b
Tyler-Breithaupt <breith61@mail.nmc.edu>
2024-10-12 16:10:36 -0400

    Add project files.

---
+++ RoutingPractice/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace RoutingPractice.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Home");
+        }
+
+        public IActionResult Privacy()
+        {
+            return Content("Privacy");
+        }
+
+        public IActionResult Display(string id)
+        {
+            if (id == null) {
+                return Content("No ID supplied.");
+            }
+            else {
+                return Content("ID: " + id);
+            }
+        }
+
+        [Route("[action]/{start}/{end?}/{message?}")]
+        public IActionResult Countdown(int start, int end = 0, string message = "")
+        {
+            StringBuilder countdown = new StringBuilder("Counting down: ");
+
+            for (int i = start; i >= end; i--)
+            {
+                countdown.Append(i + " ");
+            }
+
+            if (!string.IsNullOrEmpty(message))
+            {
+                countdown.Append(message);
+            }
+            else
+            {
+                countdown.Append("Liftoff!");
+            }
+
+            return Content(countdown.ToString());
+        }
+    }
+}
